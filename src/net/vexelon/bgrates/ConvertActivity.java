f91f232974c1ac6a86bcdfad2f7193c1fdb0083d
pro.xex@gmail.com <pro.xex@gmail.com@fea7e931-7330-ee8e-139d-207846fd6408>
2010-08-26 21:30:36 +0000

    * added - convert activity
    git-svn-id: http://bgrates.googlecode.com/svn/trunk@26 fea7e931-7330-ee8e-139d-207846fd6408

---
+++ src/net/vexelon/bgrates/ConvertActivity.java
@@ -0,0 +1,17 @@
+package net.vexelon.bgrates;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConvertActivity extends Activity {
+	
+	private final static String TAG = Defs.LOG_TAG;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//TODO:
+	}
+
+}
