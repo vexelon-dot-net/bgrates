edfb395bb2f6ddc10e6feeeefa58ec475b38b335
pro.xex@gmail.com <pro.xex@gmail.com@fea7e931-7330-ee8e-139d-207846fd6408>
2010-09-23 20:24:10 +0000

    * changed - layout of currency view list. Increased font sizes, currency code is now being shown.
    git-svn-id: http://bgrates.googlecode.com/svn/trunk@46 fea7e931-7330-ee8e-139d-207846fd6408

--- src/net/vexelon/bgrates/CurrencyListAdapter.java
+++ src/net/vexelon/bgrates/CurrencyListAdapter.java
@@ -37,22 +37,17 @@ public class CurrencyListAdapter extends ArrayAdapter<CurrencyInfo> {
 		CurrencyInfo ci = _items.get(position);
 		if ( ci != null ) {
 			
-			TextView tvCode = (TextView) v.findViewById(R.id.code);
-			if ( tvCode != null )
-				tvCode.setText(ci.getName());
+			setResText(v, R.id.name, ci.getName());
+			setResText(v, R.id.code, ci.getCode());
+			setResText(v, R.id.ratio, ci.getRatio());
+			setResText(v, R.id.rate,
+					ci.getRate().length() > Defs.MAX_RATE_CHARS_SIZE ? ci.getRate().subSequence(0, Defs.MAX_RATE_CHARS_SIZE) : ci.getRate()
+					);
 			
-			TextView tvRatio = (TextView) v.findViewById(R.id.ratio);
-			if ( tvRatio != null )
-				tvRatio.setText(ci.getRatio());
-			
-			TextView tvRate = (TextView) v.findViewById(R.id.rate);
-			if ( tvRate != null ) {
-//				String text = String.format("%s %s", 
-//						ci.getRate().length() > Defs.MAX_RATE_CHARS_SIZE ? ci.getRate().subSequence(0, Defs.MAX_RATE_CHARS_SIZE) : ci.getRate(),
-//						getContext().getResources().getString(R.string.bg_currency_name)
-//						);
-				tvRate.setText(ci.getRate().length() > Defs.MAX_RATE_CHARS_SIZE ? ci.getRate().subSequence(0, Defs.MAX_RATE_CHARS_SIZE) : ci.getRate());
-			}
+//			String text = String.format("%s %s", 
+//					ci.getRate().length() > Defs.MAX_RATE_CHARS_SIZE ? ci.getRate().subSequence(0, Defs.MAX_RATE_CHARS_SIZE) : ci.getRate(),
+//					getContext().getResources().getString(R.string.bg_currency_name)
+//					);
 			
 			// add last
 			ImageView icon = (ImageView) v.findViewById(R.id.icon);
@@ -70,5 +65,10 @@ public class CurrencyListAdapter extends ArrayAdapter<CurrencyInfo> {
 		return v;
 	}
 
+	private void setResText(View v, int id, CharSequence text) {
+		TextView tx = (TextView)v.findViewById(id);
+		if ( tx != null )
+			tx.setText(text);
+	}	
 	
 }
